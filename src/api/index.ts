d95da12ee7f892df8cfba8df6d5709d768e3094e
DodyDharma440 <dharma.dody@yahoo.com>
2021-06-24 22:26:47 +0800

    try to not use axios.create

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,35 +1,33 @@
 import axios, { AxiosResponse } from "axios";
 import { INoteForm, INoteResponse } from "interfaces/note";
 
-const apiNotes = axios.create({
-  baseURL: "https://notes-app-dody.vercel.app/api",
-});
+const apiBaseUrl = "https://notes-app-dody.vercel.app/api";
 
 export const getNotes = (): Promise<AxiosResponse<INoteResponse>> => {
-  return apiNotes.get("/notes");
+  return axios.get(`${apiBaseUrl}/notes`);
 };
 
 export const addNote = (
   formData: INoteForm
 ): Promise<AxiosResponse<INoteResponse>> => {
-  return apiNotes.post("/notes", formData);
+  return axios.post(`${apiBaseUrl}/notes`, formData);
 };
 
 export const getNote = (
   id: string | undefined | string[]
 ): Promise<AxiosResponse<INoteResponse>> => {
-  return apiNotes.get(`/note/${id}`);
+  return axios.get(`${apiBaseUrl}/note/${id}`);
 };
 
 export const deleteNote = (
   id: string
 ): Promise<AxiosResponse<INoteResponse>> => {
-  return apiNotes.delete(`/note/${id}`);
+  return axios.delete(`${apiBaseUrl}/note/${id}`);
 };
 
 export const updateNote = (
   formData: INoteForm,
   id: string
 ): Promise<AxiosResponse<INoteResponse>> => {
-  return apiNotes.put(`/note/${id}`, formData);
+  return axios.put(`${apiBaseUrl}/note/${id}`, formData);
 };
